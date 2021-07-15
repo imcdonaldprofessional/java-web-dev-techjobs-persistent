3815b2e5237dc02ecc0c385a2afda67261ced424
unknown <imcdonaldprofessional@gmail.com>
2021-07-14 22:07:24 -0500

    finished!
    Completed final assignment

--- queries.sql
+++ queries.sql
@@ -1,7 +1,21 @@
 ## Part 1: Test it with SQL
+CREATE TABLE job (
+    id int AUTO_INCREMENT primary key,
+    name varchar(100),
+    skill varchar(255),
+    employer_id int,
+    job_id
+);
 
 ## Part 2: Test it with SQL
+SELECT * from employer WHERE(location = "St Louis City");
 
 ## Part 3: Test it with SQL
+DROP TABLE job
 
 ## Part 4: Test it with SQL
+SELECT name, description
+FROM skill
+LEFT JOIN job_skills ON skill.id = job_skills.skills_id
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY NAME ASC;
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,41 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    //    @JoinColumn
+    @OneToMany(mappedBy = "employer")
+    private List<Job> jobs;
 
+    @NotBlank(message = "It has to be located somewhere.")
+    private String location;
+
+    public Employer(String location) {
+        this.location = location;
+    }
+
+    public Employer() {}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
 }
